88732276d612d21f3729d0dbd42dba4f2cb66f45
ponyspy <desade4me@gmail.com>
2014-10-07 19:26:10 +0400

    model

--- models/main.js
+++ models/main.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose'),
 			Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-		login: String,
-		password: String,
-		email: String,
-		status: {type: String, default: 'User'},
-		date: {type: Date, default: Date.now},
+	login: String,
+	password: String,
+	email: String,
+	status: {type: String, default: 'User'},
+	date: {type: Date, default: Date.now},
 });
 
 var postSchema = new Schema({
