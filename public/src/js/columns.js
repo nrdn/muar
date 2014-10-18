14594c8f3d0354669fc9cc93f58cfa5ddc70e932
plnttr1003 <top@nrdn.ru>
2014-10-18 13:57:54 +0400

    columns sort start

--- public/src/js/columns.js
+++ public/src/js/columns.js
@@ -1,29 +1,46 @@
-var col_n = 0; // Количество колонок
-var m_left = 0; // Отступ блока с колонками слева
-var col_vis = 4; // Количество видимых колонок
+var st_cont_l = 0; // Количество стилей
+var st_vis = 1; // Количество видимых стилей
+var style_num = 2; //Номер стиля при загрузке
+var styles_container_margin = 0; //Отступ контейнера стилей
+var styles_container_width = 0; // Ширина контейнера стилей
+
 $(document).ready(function() {
-    var per_l = $('.styles_container .periods_col').length-col_vis;
-    var m_left_r = $('.styles_container').width()+3;
-    console.log(per_l+' '+m_left_r);
+		var styles_block_width = $('.content_block_styles').width();	// Ширина контейнера стиля
+		var styles_containers_length = $('.styles_container').length; // Количество контейнеров стилей
+
+		function show_styles(style_num) {
+			$('.styles_container').each(function(index){
+				styles_container_width = $(this).width();
+				if (style_num>index) {
+					styles_container_margin = styles_container_margin + styles_container_width;
+				}
+				$(this).attr({'data-length': styles_container_width}).css({'min-width':styles_container_width});
+				if (styles_container_width > styles_block_width) {
+					$(this).addClass('inner_blocks');
+				};
+			});
+			$('.content_block_styles').css({'margin-left':-styles_container_margin});
+			//console.log(styles_container_margin);
+			$('.content_title').text($('.styles_container').eq(style_num).attr('data_style_name'));
+		};
+		show_styles(style_num);
 
 
-    $('.styles_controls_front').click(function(event) {
-    	console.log('212'+' '+col_n+' '+per_l);
-    	if (col_n >= 0 && col_n < per_l) {
-    		m_left = m_left - m_left_r;
-    		console.log (m_left);
-    		$('.content_block_styles').css({'margin-left': m_left});
-    		col_n = col_n+1;
-			}
-		});
+	$('.styles_controls_front').on('click', function() {
+		styles_container_margin = 0;
+  	if (style_num < styles_containers_length - st_vis) {
+  		style_num = ++style_num;
+  		//console.log(style_num);
+			show_styles(style_num);
+		}
+	});
 
-    $('.styles_controls_back').click(function(event) {
-    	console.log('312'+' '+col_n+' '+per_l);
-    	if (col_n > 0 && col_n <= per_l) {
-	    	m_left = m_left + m_left_r;
-	    	console.log (m_left);
-	    	$('.content_block_styles').css({'margin-left': m_left});
-	    	col_n = col_n-1;
-    	}
-    });
+	$('.styles_controls_back').on('click', function() {
+		styles_container_margin = 0;
+		if (style_num > 0) {
+			style_num = --style_num;
+			//console.log(style_num);
+			show_styles(style_num);
+		}
+	});
 });
