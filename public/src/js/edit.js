a85cbe09ba360c90994b7747be7760e36dca50a0
ponyspy <desade4me@gmail.com>
2014-10-21 20:13:44 +0400

    admin

--- public/src/js/edit.js
+++ public/src/js/edit.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 	$('.form_description').popline({disable:['color']});
 	$('.form_images_second').sortable({placeholder: 'column_placeholder', cancel: '.image_second_description'});
-	$('.form_images_maps').sortable({placeholder: 'column_placeholder', cancel: '.image_maps_description'});
 
-	$(document).on('dblclick', '.image_second_preview, .image_maps_preview', function() {
+
+	$(document).on('dblclick', '.image_second_preview', function() {
 		$(this).remove();
 	});
 
@@ -72,56 +72,8 @@ $(document).ready(function() {
 	});
 
 
-	$('.form_images_maps').filedrop({
-		url: '/upload',
-		paramname: 'image',
-		// fallback_id: 'images_second_fallback',
-		allowedfiletypes: ['image/jpeg','image/png','image/gif'],
-		allowedfileextensions: ['.jpg','.jpeg','.png','.gif'],
-		maxfiles: 5,
-		maxfilesize: 8,
-		dragOver: function() {
-			$(this).css('outline', '2px solid red');
-		},
-		dragLeave: function() {
-			$(this).css('outline', 'none');
-		},
-		uploadStarted: function(i, file, len) {
-
-		},
-		uploadFinished: function(i, file, response, time) {
-			var image = $('<div />', {'class': 'image_maps_preview', 'style': 'background-image:url(' + response + ')'});
-			var description = $('<div />', {'class': 'image_maps_description', 'contenteditable': true, 'text':'Описание'});
-			$('.form_images_maps').append(image.append(description));
-			console.log(response);
-		},
-		progressUpdated: function(i, file, progress) {
-
-		},
-		afterAll: function() {
-			$('.form_images_maps').css('outline', 'none');
-		}
-	});
-
-
-
-
-	// $('.footer_block').click(function(event) {
-
-	// 	var old = $('.form_old').is(':checked') ? true : false
-
-	// 	console.log(old)
-	// });
-
-
-
-
-
-
-
 	$('.submit').click(function(event) {
 		var images_second_upload = [];
-		var images_maps_upload = [];
 
 		var title = $('.form_title').html();
 		var description = $('.form_description').html();
@@ -130,7 +82,6 @@ $(document).ready(function() {
 
 		var images_main = $('.form_image_main').attr('style').match(/\(([^)]+)\)/)[0].slice(1,-1).replace('http://' + window.location.host, '');
 		var images_second = $('.image_second_preview');
-		var images_maps = $('.image_maps_preview');
 
 
 		images_second.each(function(index, el) {
@@ -140,19 +91,9 @@ $(document).ready(function() {
 			});
 		});
 
-
-		images_maps.each(function(index, el) {
-			images_maps_upload.push({
-				path: $(this).attr('style').match(/\(([^)]+)\)/)[0].slice(1,-1).replace('http://' + window.location.host, ''),
-				description: $(this).children('.image_maps_description').text()
-			});
-		});
-
-
 		var images = {
 			main: images_main,
-			second: images_second_upload,
-			maps: images_maps_upload
+			second: images_second_upload
 		};
 
 
@@ -167,11 +108,7 @@ $(document).ready(function() {
 			images: images,
 			category: category
 		}).done(function(project) {
-
-
 			window.location.reload();
-
-
 		});
 	});
 });
