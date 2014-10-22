3b0e2b5e8c75594c0be36eb041149c094e8feb27
plnttr1003 <top@nrdn.ru>
2014-10-22 18:30:38 +0400

    object

--- public/src/js/columns2.js
+++ public/src/js/columns2.js
@@ -13,17 +13,23 @@ $(document).ready(function() {
 		}
 
 	function show_styles(style_num) {
-			console.log(style_num);
 				$('.period').eq(style_num).addClass('active');
 				var $this_st = $('.styles_container').eq(style_num);
-				console.log($this_st.attr('data-scroll-top'))
 				$('body').animate({scrollTop : $this_st.attr('data-scroll-top')}, 250)
 		}
 
 		show_styles(style_num);
 
 		$('.period').on('click', function() {
-			$('.period').removeClass('active')
+			$('.period').removeClass('active');
+			//$(this).width(400);
 			show_styles($(this).index());
+			var t_o = $(this).offset().left;
+			var t_wi = $(this).width() ;
+			var t_wi_p = t_wi/2;
+			var per_m = 640 - t_wi_p - t_o;
+			console.log(t_wi + ' opop: ' + t_wi_p + 'oop:' +per_m);
+			//$('.periods').css({'margin-left':per_m});
+			//console.log('Оффсет: ' + $(this).offset().left + ' Ширина: ' + $(this).width() + ' Отступ периодов: ' + (640 - ($(this).width()/2)));
 		})
 })
