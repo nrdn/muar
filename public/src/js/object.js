751f4acc0d090a38f143bb69998a8c66fb9763f0
plnttr1003 <top@nrdn.ru>
2014-10-23 21:43:17 +0400

    object

---
+++ public/src/js/object.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$('.project_image_main_inner').width($('.project_image_main').width());
+	$('.project_image_main_inner').height($('.project_image_main').height())
+})
