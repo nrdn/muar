cffce2bddb900e6042a3e6a3c9b01b6aec8192f9
ponyspy <desade4me@gmail.com>
2014-11-10 01:06:54 +0300

    object

--- public/src/js/object.js
+++ public/src/js/object.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-	$('.image_second_block').click(function(event) {
-		console.log($(this).index());
-		//$('.project_image_main').css({'margin-top':'-100%'});
-		$('.project_image_second').addClass('image_animate').css({'margin-top':'-100%','z-index':'0'}).eq($(this).index()).removeClass('image_animate').css({'margin-top':'0','z-index':'-1'});
-	})
-})
+	$('.description').on('click', function(event) {
+		$('.object_description_block').show();
+	});
+});
