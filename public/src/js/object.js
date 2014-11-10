9bf1e0b5ae96d61aeafafc5c8684aecdfd93f772
ponyspy <desade4me@gmail.com>
2014-11-10 14:00:12 +0300

    subject

--- public/src/js/object.js
+++ public/src/js/object.js
@@ -7,8 +7,25 @@ $(document).ready(function() {
 		$('.object_images_navigate').show();
 	});
 
+	$('.subjects').click(function(event) {
+		$('.object_images_block').hide();
+		$('.object_subjects_block').show();
+		var map = L.map('subjects_view').setView([0, 0], 1);
+
+		L.tileLayer('/tiles/{z}/image_tile_{y}_{x}.jpg',{
+			minZoom: 1,
+			maxZoom: 4,
+			attribution: '',
+			tileSize: '256',
+			tms: false,
+			continuousWorld: true
+		}).addTo(map);
+	});
+
 	$('.object_image_navigate').on('click', function(event) {
 		var index = $(this).index();
+		$('.object_images_block').show();
+		$('.object_subjects_block').hide();
 		$('.object_image').hide().eq(index).show();
 	});
 
