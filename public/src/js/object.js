b553039042850f895074ff70e75004efc8807ed8
ponyspy <desade4me@gmail.com>
2014-11-10 12:30:59 +0300

    object

--- public/src/js/object.js
+++ public/src/js/object.js
@@ -3,8 +3,17 @@ $(document).ready(function() {
 		$('.object_description_block').show();
 	});
 
+	$('.images').on('click', function(event) {
+		$('.object_images_navigate').show();
+	});
+
+	$('.object_image_navigate').on('click', function(event) {
+		var index = $(this).index();
+		$('.object_image').hide().eq(index).show();
+	});
+
 	$(document).mouseup(function (event) {
-		var container = $('.object_description_block');
+		var container = $('.object_description_block, .object_images_navigate');
 
 		if (!container.is(event.target)
 			&& container.has(event.target).length === 0)
