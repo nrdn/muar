39d655feebcb91fcacc1ebb3c687033408a5d185
ponyspy <desade4me@gmail.com>
2014-11-10 12:03:54 +0300

    styles

--- public/src/js/object.js
+++ public/src/js/object.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
 	$('.description').on('click', function(event) {
 		$('.object_description_block').show();
 	});
+
+	$(document).mouseup(function (event) {
+		var container = $('.object_description_block');
+
+		if (!container.is(event.target)
+			&& container.has(event.target).length === 0)
+		{
+				container.hide();
+		}
+	});
 });
