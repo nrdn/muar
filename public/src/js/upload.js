506e8189ba8de30b16304d657c3521aa50ce4291
ponyspy <desade4me@gmail.com>
2014-11-15 22:33:03 +0300

    styles

--- public/src/js/upload.js
+++ public/src/js/upload.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
-	$('.form_images_upload').sortable({placeholder: 'column_placeholder', cancel: '.image_upload_description'});
+	$('.form_images_upload').sortable({placeholder: 'column_placeholder', cancel: '.image_upload_description, .image_upload_delete'});
+
+	$(document).on('click', '.image_upload_delete', function(event) {
+		$(this).closest('.image_upload_block').remove();
+	});
 
 	$('.form_images_upload').filedrop({
 		url: '/preview',
 		paramname: 'image',
-		// fallback_id: 'images_second_fallback',
+		fallback_id: 'images_upload_fallback',
 		allowedfiletypes: ['image/jpeg','image/png','image/gif'],
 		allowedfileextensions: ['.jpg','.jpeg','.png','.gif'],
 		maxfiles: 5,
@@ -21,9 +25,10 @@ $(document).ready(function() {
 		uploadFinished: function(i, file, response, time) {
 			var image_upload_block = $('<div />', {'class': 'image_upload_block'});
 			var image = $('<div />', {'class': 'image_upload_preview', 'style': 'background-image:url(' + response + ')'});
+			var image_delete = $('<div />', {'class': 'image_upload_delete', 'text': 'Удалить'});
 			var description = $('<textarea />', {'class': 'image_upload_description', 'name': 'images[description]', 'text':'Описание'});
 			var form = $('<input />', {'class': 'image_upload_form', 'type': 'hidden', 'name': 'images[path]', 'value': response});
-			$('.form_images_upload').append(image_upload_block.append(image, description, form));
+			$('.form_images_upload').append(image_upload_block.append(image.append(image_delete), description, form));
 		},
 		progressUpdated: function(i, file, progress) {
 
