7dcdde86062f8db97c0506e8956060fe8998f990
plnttr1003 <top@nrdn.ru>
2014-11-25 17:01:08 +0300

    design

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -10,15 +10,4 @@ $(document).ready(function() {
 		freezeClass: 'freeze'
 	});
 
-	$('.parallax_block').on({
-		mouseover: function() {
-			$('.intro_title').css('color', 'red');
-		},
-		mouseout: function() {
-			$('.intro_title').removeAttr('style');
-		},
-		click: function() {
-			document.location.href = '/styles';
-		}
-	});
 });
