f537d590f9c1e0492ae3d7ee46ff6166bfa2552d
plnttr1003 <top@nrdn.ru>
2014-11-25 16:29:58 +0300

    design

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
-	$('.parallax-layer').parallax({
-		mouseport: $('.parallax_block'),
+	var scale = $(document).width()/1280;
+	$('.parallax-layer')
+	.css({
+		'transform-origin':'50%',
+		'transform':'scale(' + scale + ')'
+	})
+	.parallax({
+		mouseport: $('.parallax_block, .slides_arrows_block'),
 		freezeClass: 'freeze'
 	});
 
