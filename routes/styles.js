a7a027a4ad044a8dbb087d20fe2f8dc732bbe683
ponyspy <desade4me@gmail.com>
2014-12-10 02:01:18 +0300

    plugin

--- routes/styles.js
+++ routes/styles.js
@@ -1,10 +1,13 @@
 var Age = require('../models/main.js').Age;
 var Object = require('../models/main.js').Object;
+var Project = require('../models/main.js').Project;
 
 exports.index = function(req, res) {
 	Age.find().where('parent').exists(false).sort('meta.interval.start meta.interval.end').select('-date -__v').exec(function(err, ages) {
 		Age.populate(ages, {path: 'sub', select: '-date -__v -parent -sub', options: {sort: 'meta.interval.start meta.interval.end'}}, function(err, ages) {
-			res.render('styles/index.jade', {ages: ages});
+			Project.find().exec(function(err, projects) {
+				res.render('styles/index.jade', {ages: ages, projects: projects});
+			});
 		});
 	});
 }
