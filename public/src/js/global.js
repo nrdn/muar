d0c33d246fa184eb5e33c70af78dc0a7c9bed2ff
ponyspy <desade4me@gmail.com>
2015-04-13 18:36:26 +0300

    search

--- public/src/js/global.js
+++ public/src/js/global.js
@@ -1,14 +1,32 @@
+function getCookie(name) {
+	var matches = document.cookie.match(new RegExp(
+		"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+	));
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function getProperty(field, locale) {
+	var value = field.filter(function(v) {
+		return v.lg === locale;
+	})[0].value;
+
+	return value;
+}
+
 $(document).ready(function() {
+	var locale = getCookie('locale') || 'ru';
+
 	var menu_navigation = function menu_navigation() {
-	 var scroll_top = $(document).scrollTop();
-			if (scroll_top > 150) {
-					$('.header_block').addClass('fixed_header');
-					$('body').addClass('scrolled_page');
-				} else {
-					$('.header_block').removeClass('fixed_header');
-					$('body').removeClass('scrolled_page');
-				}
-			}
+	var scroll_top = $(document).scrollTop();
+	if (scroll_top > 150) {
+			$('.header_block').addClass('fixed_header');
+			$('body').addClass('scrolled_page');
+		} else {
+			$('.header_block').removeClass('fixed_header');
+			$('body').removeClass('scrolled_page');
+		}
+	}
+
 	$(document).on('scroll', menu_navigation)
 
 
@@ -25,17 +43,17 @@ $(document).ready(function() {
 				$('.objects_context, .architects_context, .subjects_context').hide().children('.context_results_block').empty();
 
 				data.objects.forEach(function(object) {
-					var context_result = $('<a/>', {'class': 'context_result', 'href': '/objects/' + object._id, 'text': object.title[0].value});
+					var context_result = $('<a/>', {'class': 'context_result', 'href': '/objects/' + object._id, 'text': getProperty(object.title, locale) });
 					$('.objects_context').show().children('.context_results_block').append(context_result);
 				});
 
 				data.architects.forEach(function(architect) {
-					var context_result = $('<a/>', {'class': 'context_result', 'href': '/architects/' + architect._id, 'text': architect.name[0].value});
+					var context_result = $('<a/>', {'class': 'context_result', 'href': '/architects/' + architect._id, 'text': getProperty(architect.title, locale) });
 					$('.architects_context').show().children('.context_results_block').append(context_result);
 				});
 
 				data.subjects.forEach(function(subject) {
-					var context_result = $('<a/>', {'class': 'context_result', 'href': '/subjects/' + subject._id, 'text': subject.title[0].value});
+					var context_result = $('<a/>', {'class': 'context_result', 'href': '/subjects/' + subject._id, 'text': getProperty(subject.title, locale) });
 					$('.subjects_context').show().children('.context_results_block').append(context_result);
 				});
 			});
