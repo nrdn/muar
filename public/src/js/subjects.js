c74fafbe2a0378b1aea9d0ea2549ab94b827d9ae
plnttr1003 <top@nrdn.ru>
2015-05-17 21:11:45 +0300

    subjects

--- public/src/js/subjects.js
+++ public/src/js/subjects.js
@@ -4,13 +4,17 @@ $(document).ready(function() {
 		window.location.hash = $(this).index();
 	}
 	function loadSubjects(technique_id) {
+		$('.technique_sort').removeClass('current');
+		$('.subject_item').hide();
 			console.log('tech' + technique_id);
-			if (technique_id == '') {technique_id_nth = 1}
-			else {technique_id_nth = parseInt(technique_id)+1}
-			$('.technique_sort').removeClass('current');
-			$('.technique_sort:nth-child(' + technique_id_nth + ')').addClass('current');
-			$('.subject_item').hide();
-			$('.subject_item[data-technique='+ $('.technique_sort').eq(technique_id).attr('data-technique') +']').show();
+			if (technique_id == '' || technique_id == 0) {
+				$('.subject_item').show();
+				$('.technique_skip').addClass('current');
+			}
+			else {technique_id_nth = parseInt(technique_id)+1
+				$('.technique_sort:nth-child(' + technique_id_nth + ')').addClass('current');
+				$('.subject_item[data-technique='+ $('.technique_sort').eq(technique_id).attr('data-technique') +']').show();
+			}
 	}
 	function hashchange_func() {
 		var technique_id = window.location.hash.replace('#','');
